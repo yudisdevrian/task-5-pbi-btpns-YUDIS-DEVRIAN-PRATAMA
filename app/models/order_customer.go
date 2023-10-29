d56d804d9c82dccd3e71edf5e52e8da01b78567f
yudisseedvp <94956760+yudisdevrian@users.noreply.github.com>
2023-10-29 20:05:54 +0700

    Add files via upload

---
+++ app/models/order_customer.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type OrderCustomer struct {
+	ID         string `gorm:"size:36;not null;uniqueIndex;primary_key"`
+	User       User
+	UserID     string `gorm:"size:36;index"`
+	Order      Order
+	OrderID    string `gorm:"size:36;index"`
+	FirstName  string `gorm:"size:100;not null"`
+	LastName   string `gorm:"size:100;not null"`
+	CityID     string `gorm:"size:100;"`
+	ProvinceID string `gorm:"size:100;"`
+	Address1   string `gorm:"size:100;"`
+	Address2   string `gorm:"size:100;"`
+	Phone      string `gorm:"size:50;"`
+	Email      string `gorm:"size:100;"`
+	PostCode   string `gorm:"size:100;"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
+
+func (o *OrderCustomer) BeforeCreate(db *gorm.DB) error {
+	if o.ID == "" {
+		o.ID = uuid.New().String()
+	}
+
+	return nil
+}
